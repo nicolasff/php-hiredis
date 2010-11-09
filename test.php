5aa2d78e40065084d33b045c4b062640c8ed404d
Nicolas Favre-Felix <n.favrefelix@gmail.com>
2010-11-09 21:15:10 +0100

    Added micro-benchmark.

--- test.php
+++ test.php
@@ -2,7 +2,20 @@
 
 $r = new HiRedis;
 var_dump($r->connect('127.0.0.1'));
-var_dump($r->set('key', 'value'));
-var_dump($r->get('key'));
+
+$count = 10000;
+
+$t0 = microtime(true);
+for($i = 0; $i < $count; $i++) {
+	$r->set('key', 'value');
+}
+$t1 = microtime(true);
+for($i = 0; $i < $count; $i++) {
+	$r->get('key');
+}
+$t2 = microtime(true);
+
+printf("%d sets: %0.2f sec (%d/sec)\n", $count, $t1-$t0, ($count)/($t1-$t0));
+printf("%d gets: %0.2f sec (%d/sec)\n", $count, $t2-$t1, ($count)/($t2-$t1));
 
 ?>
